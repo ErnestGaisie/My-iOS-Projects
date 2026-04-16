b13f68eb3eb218c83674a2bc22c4386af0fd6642
Ernest Gaisie <ernestyawgaisie@gmail.com>
2026-04-15 22:58:08 -0400

    Building a list we can delete from

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Ernest Boamah Gaisie on 2026-04-14.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus"){
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    
+                    withAnimation {
+                        expenses.items.append(expense)
+                    }
+                }
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ernest Boamah Gaisie on 2026-04-14.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
